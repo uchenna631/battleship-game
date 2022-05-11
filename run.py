8e8e5fd9fd3e47cde302e8f3078481bc8ed0fa73
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-05-11 01:13:01 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,46 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+scores = {'computer': 0, 'player': 0}
+
+# Board class adopted and modified from the CI's battleship tutorial
+class Board:
+    """
+    Main board class. Sets board size, the number of ships,
+     the player's name and the board type (player board or computer).
+     Has methods for adding ships and guesses and printing the board
+     """
+
+
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type  = type
+        self.guesses = []
+        self.ships = []
+
+
+    def print(self):
+        # prints board
+        for row in self.board:
+            print("  ".join(row))
+
+    def guess(self, x, y):
+        # appends X at the chosen coordinates
+        self.board[x][y] = "X"
+
+        # appends * if chosen coordinates hits a target
+        if (x, y) in self.ships:
+            self.board[x][y] = "*"
+            return "Hit"
+        else:
+            return "Miss"
+
+    def add_ship(self, x, y, type="computer"):
+        if len(self.ships) >= self.num_ships:
+            print("Error: you cannot add any more ships!")
+        else:
+            self.ships.append((x, y))
+            if self.type == "player":
+                self.board[x][y] = "@"
