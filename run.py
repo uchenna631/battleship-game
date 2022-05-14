98bb6ca6023db440eb6a0b8d26141a423e103107
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-05-14 15:32:34 +0000

    Fix the board size error message in validate_coordinate function

--- run.py
+++ run.py
@@ -60,7 +60,7 @@ def validate_coordinates(x, y, board):
         board.board[x][y] in board.board
 
     except IndexError:
-        print(f"Invalid data: row and column must be an integer between 0 - {board.size}")
+        print(f"Invalid data: row and column must be an integer between 0 - {board.size - 1}")
         return False
 
     except ValueError:
